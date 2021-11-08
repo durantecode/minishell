1aa925a1c4797f2baa9464931e1597dff89bf7c2
Daniel Pavon gomez <dpavon-g@c1r8s4.42madrid.com>
2021-11-08 15:43:43 +0100

    test

--- srcs/main.c
+++ srcs/main.c
@@ -6,22 +6,30 @@
 /*   By: dpavon-g <dpavon-g@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/08 13:03:38 by ldurante          #+#    #+#             */
-/*   Updated: 2021/11/08 15:01:18 by dpavon-g         ###   ########.fr       */
+/*   Updated: 2021/11/08 15:43:31 by dpavon-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
+void	leaks()
+{
+	system("leaks minishell");
+}
+
 int		main(void)
 {
-    char    *user;
-    char    *prompt;
+	char    *prompt;
+	char	*string;
 
-    user = getenv("USER");
-    prompt = ft_strjoin(user, "@minishell% ");
-    while (1)
-    {
-        readline(prompt);
-    }
-    free(prompt);
+	atexit(leaks);
+	prompt = ft_strjoin(getenv("USER"), "@minishell% ");
+	while (1)
+	{
+		string = readline(prompt);
+		free(string);
+	}
+	free(prompt);
 }
+
+prueba
