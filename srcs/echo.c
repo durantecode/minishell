aaa002facdef2ef4ae795821d6c828989468af24
Luis Durante <durante@Mini-de-Luis.JOOWIN>
2021-11-26 02:15:03 +0100

    delete garbage

--- srcs/echo.c
+++ srcs/echo.c
@@ -6,20 +6,11 @@
 /*   By: ldurante <ldurante@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 12:33:35 by ldurante          #+#    #+#             */
-/*   Updated: 2021/11/25 00:27:46 by ldurante         ###   ########.fr       */
+/*   Updated: 2021/11/26 00:42:14 by ldurante         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
-
-void	expand_var(char *str, t_input *in)
-{
-	char	*var;
-
-	var = ft_getenv(str, in);
-	if (var)
-		printf("%s ", var);
-}
 
 void	echo(t_input *in)
 {
