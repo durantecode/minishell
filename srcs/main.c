f555d9282fe8471db4d38eda7fd128d097ec6b3d
Daniel Pavon gomez <dpavon-g@c1r17s3.42madrid.com>
2021-11-27 07:43:23 +0100

    minishell environ

--- srcs/main.c
+++ srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ldurante <ldurante@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dpavon-g <dpavon-g@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/08 13:03:38 by ldurante          #+#    #+#             */
-/*   Updated: 2021/11/26 15:58:50 by ldurante         ###   ########.fr       */
+/*   Updated: 2021/11/27 07:40:07 by dpavon-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,25 +36,41 @@ void	init_structs(t_input *in, t_list **envp)
 	in->cmd_path = NULL;
 }
 
+void sigint_handler(int sig) 
+{
+	(void)sig;
+	write(1, &"\n", 1);
+    return;
+}
+
 int	main(void)
 {
 	t_input				in;
 	t_list				*envp;
-	struct sigaction	sa;
+	//struct sigaction	sa;
 
-	// atexit(leaks);
-	if (*environ)
+	atexit(leaks);
+	if (!(*environ))
 	{
-		envp = NULL;
-		init_env_list(&envp);
-		init_structs(&in, &envp);
-		sa.sa_sigaction = catch_signal;
-		while (1)
-		{
-			sigaction(SIGINT, &sa, NULL);
-			read_input(&in);
-		}
-		return (0);
+		environ = malloc(sizeof(char *) * 5);
+		environ[0] = ft_strjoin("PWD=", getcwd(NULL, 0));
+		environ[1] = ft_strdup("SHLVL=1");
+		environ[2] = ft_strdup("_=/usr/bin/env");
+		environ[3]
+			= ft_strdup("PATH=/usr/gnu/bin:/usr/local/bin:/bin:/usr/bin:.");
+		environ[4] = NULL;
 	}
-	// printf("%s\n", getenv("PATH"));
+	envp = NULL;
+	init_env_list(&envp);
+	init_structs(&in, &envp);
+	//sa.sa_sigaction = catch_signal;
+	while (1)
+	{
+		//sigaction(SIGINT, &sa, NULL);
+		signal(SIGQUIT, SIG_IGN);
+		signal(SIGINT, SIG_IGN);
+		read_input(&in);
+		//signal(SIGQUIT, SIG_IGN);
+	}
+	return (0);
 }
