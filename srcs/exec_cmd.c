a972f358c4cda650a7718d8a85a70915485fc086
Daniel Pavon gomez <dpavon-g@c3r7s2.42madrid.com>
2021-11-29 13:43:50 +0100

    Executable changes

--- srcs/exec_cmd.c
+++ srcs/exec_cmd.c
@@ -3,35 +3,43 @@
 /*                                                        :::      ::::::::   */
 /*   exec_cmd.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ldurante <ldurante@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dpavon-g <dpavon-g@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 12:29:09 by ldurante          #+#    #+#             */
-/*   Updated: 2021/11/24 23:43:20 by ldurante         ###   ########.fr       */
+/*   Updated: 2021/11/29 13:39:46 by dpavon-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-/*
-	Esta funcion hay que arreglarla porque si el archivo existe pero no es un ejecutable
-	intenta ejecutar y crea un nuevo fork.
-	Hay que hacer algo para comprobar antes si se puede ejecutar, o hacer el execve de otra forma
-	para poder pasarle la ruta solo como argumento y no como path.
-*/
 void	exec_absolute(t_input *in)
 {
 	pid_t	pid;
+	DIR 	*dir;
 
 	if ((access(in->split_input[0], F_OK)) == 0)
 	{
-		pid = fork();
-		if (pid == 0)
-			execve(in->split_input[0], in->split_input, environ);
-		waitpid(pid, NULL, 0);
+		dir = opendir(in->split_input[0]);
+		if (dir)
+		{
+			printf("minishell: %s: is a directory\n", in->split_input[0]);
+			closedir(dir);
+		}
+		else
+		{
+			if ((access(in->split_input[0], X_OK)) == 0)
+			{
+				pid = fork();
+				if (pid == 0)
+					execve(in->split_input[0], in->split_input, environ);
+				waitpid(pid, NULL, 0);
+			}
+			else
+				printf("minishell: %s: Permission denied\n", in->split_input[0]);
+		}
 	}
 	else
 		printf("minishell: %s: No such file or directory\n", in->split_input[0]);
-	(void)pid;
 }
 
 void	exec_cmd(t_input *in)
