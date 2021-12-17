35a21f96ea4b7d47e116025b95dbc2a84f27ff6c
Luis Durante <durante@Mini-de-Luis.JOOWIN>
2021-12-17 11:38:07 +0100

    unsetttt

--- srcs/builtins/unset.c
+++ srcs/builtins/unset.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   unset.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dpavon-g <dpavon-g@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ldurante <ldurante@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 13:30:46 by ldurante          #+#    #+#             */
-/*   Updated: 2021/12/15 16:18:40 by dpavon-g         ###   ########.fr       */
+/*   Updated: 2021/12/17 11:37:28 by ldurante         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,6 @@ void	unset(t_input *in, int j)
 	char	*var;
 	int		size_var;
 	t_list	*aux;
-	t_list	*tmp;
 
 	aux = *in->env_list;
 	if (in->split_input[1] == NULL)
@@ -45,15 +44,10 @@ void	unset(t_input *in, int j)
 	}
 	while (in->split_input[j])
 	{
-		//var = ft_strjoin(in->split_input[j], "=");
 		var = ft_strdup(in->split_input[j]);
 		size_var = ft_strlen(var);
 		if (!(ft_strncmp(var, aux->content, size_var)))
-		{
-			tmp = *in->env_list;
 			*in->env_list = (*in->env_list)->next;
-			// ft_lstclear(&tmp, free); ---> REVISAR LEAKS
-		}
 		else
 			unset_aux(aux, var, size_var);
 		if (!(ft_strncmp(var, "PATH=", size_var)))
@@ -63,5 +57,4 @@ void	unset(t_input *in, int j)
 		free_matrix(in->dup_env);
 		in->dup_env = list_to_matrix(*in->env_list);
 	}
-	// print_matrix(in->dup_env);
 }
