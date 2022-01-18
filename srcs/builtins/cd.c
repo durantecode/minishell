8e175c4856f42894e12ae19a12455e876e9e9adc
Luis Durante <ldurante@c3r14s4.42madrid.com>
2022-01-18 15:28:42 +0100

    fixed cd

--- srcs/builtins/cd.c
+++ srcs/builtins/cd.c
@@ -6,11 +6,31 @@
 /*   By: ldurante <ldurante@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/10 12:33:39 by ldurante          #+#    #+#             */
-/*   Updated: 2022/01/18 13:42:06 by ldurante         ###   ########.fr       */
+/*   Updated: 2022/01/18 15:27:10 by ldurante         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
+
+void	cd_aux(t_input *in, char **pwd, char **home_path)
+{
+	if (*pwd)
+		update_env_var(in, "OLDPWD=", *pwd);
+	else
+	{
+		update_env_var(in, "OLDPWD=", "");
+		if (errno)
+		{	
+			chdir("/");
+			*pwd = getcwd(NULL, 0);
+		}	
+	}
+	free(*pwd);
+	*pwd = getcwd(NULL, 0);
+	update_env_var(in, "PWD=", *pwd);
+	free(*home_path);
+	free(*pwd);
+}
 
 void	cd(t_input *in)
 {
@@ -23,8 +43,6 @@ void	cd(t_input *in)
 	{
 		if (chdir(home_path) == -1)
 			error_msg(in, ERR_HOME, 0, 0);
-		else
-			update_env_var(in, "OLDPWD=", pwd);
 	}
 	else if (chdir(in->split_in[1]) != 0)
 	{
@@ -32,23 +50,6 @@ void	cd(t_input *in)
 			error_msg(in, ERR_PERM, 0, 0);
 		else
 			error_msg(in, ERR_FILE, 1, 0);
-		if (!pwd)
-		{
-			update_env_var(in, "OLDPWD=", "");
-			chdir("/");
-			pwd = getcwd(NULL, 0);
-		}
 	}
-	else
-	{
-		if (pwd)
-			update_env_var(in, "OLDPWD=", pwd);
-		else
-			update_env_var(in, "OLDPWD=", "");
-	}
-	free(pwd);
-	pwd = getcwd(NULL, 0);
-	update_env_var(in, "PWD=", pwd);
-	free(home_path);
-	free(pwd);
+	cd_aux(in, &pwd, &home_path);
 }
